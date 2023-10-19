56535ea64d7f666c84e8e2cade1b5fac14d6d607
Uma516 <hashmitha143@gmail.com>
2023-10-19 23:31:17 +0530

    css locator program commit

---
+++ src/main/java/com/nit/LocatorsWithCssSelector.java
@@ -0,0 +1,33 @@
+package com.nit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class LocatorsWithCssSelector {
+
+    public static void main(String[] args) {
+
+                ChromeOptions options=new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                WebDriver driver=new ChromeDriver(options);
+                driver.get("https://www.amazon.com/");
+                driver.manage().window().maximize();
+                //css tag with id
+                // driver.findElement(By.cssSelector("input#twotabsearchtextbox")).sendKeys("dictionary");
+                // driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("dictionary");
+
+                //tag class
+                // driver.findElement(By.cssSelector("input.nav-input")).sendKeys("dates");
+                //driver.findElement(By.cssSelector(".nav-input")).sendKeys("dates");
+                //tag with attribute
+                driver.findElement(By.cssSelector("input[name='auto']")).sendKeys("toys");
+            }
+        }
+
+    }
+}
