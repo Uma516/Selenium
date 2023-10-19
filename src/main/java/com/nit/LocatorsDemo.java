ee4852b67311af18181a9e8d477f15349ba85fd0
Uma516 <hashmitha143@gmail.com>
2023-10-19 21:06:50 +0530

    Tag name locators program commit

--- src/main/java/com/nit/LocatorsDemo.java
+++ src/main/java/com/nit/LocatorsDemo.java
@@ -10,28 +10,19 @@ import java.util.List;
 
 public class LocatorsDemo {
 
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
         ChromeOptions options=new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver=new ChromeDriver(options);
         driver.get("https://www.amazon.com/");
         driver.manage().window().maximize();
-        List<WebElement> sliders=driver.findElements(By.className("homeslider-container"));
-        System.out.println("Number of sliders:"+sliders.size());
-
-
-
-        List<WebElement> images=driver.findElements(By.tagName("img"));
-        System.out.println("Total number of images:"+images.size());
-
-
-        List<WebElement> links=driver.findElements(By.tagName("a"));
-        System.out.println("Total number of links:"+ links.size());
-
-
-        driver.quit();
-
+       List<WebElement>slides=driver.findElements(By.className("a-carousel-card"));
+        System.out.println("Number of slides="+slides.size());
+        List<WebElement>images=driver.findElements(By.tagName("img"));
+        System.out.println("Total number of images:="+images.size());
+        List<WebElement>link=driver.findElements(By.tagName("a"));
+        System.out.println("Total number of links:="+link.size());
+         driver.quit();
 
     }
 }
